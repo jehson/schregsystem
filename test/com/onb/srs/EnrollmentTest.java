a93278685989f603411969532259156595ff0192
Mon <mon.carandang@orangeandbronze.com>
2012-06-26 10:05:44 +0800

    Refactored EnrollmentTest.java

--- test/com/onb/srs/EnrollmentTest.java
+++ test/com/onb/srs/EnrollmentTest.java
@@ -25,13 +25,7 @@ public class EnrollmentTest {
 	
 	private Curriculum bsMath;
 	private Student student;
-	private Schedule schedule;
-	
-	private Section math1SectionA;
-	private Section math2SectionA;
-	
-	private ClassCard cc1;
-	private ClassCard cc2;
+	private Schedule mondayAtTen;
 
 	private EnrollmentForm firstTermEnrollmentForm;
 	
@@ -44,38 +38,37 @@ public class EnrollmentTest {
 		mrOcelot = new Teacher(002);
 		
 		math1 = new Subject("Math 1");
-		math2 = new Subject("Math 2", math1);
 		eng1 = new Subject("English 1");
 		
-		schedule = new Schedule(DaySlot.MonThu, TimeSlot.EightThirtyToTen);
-		
-		math1SectionA = new Section(1, math1, schedule, mrNarwhal);
-		math2SectionA = new Section(2, eng1, schedule, mrOcelot);
-		
-		cc1 = new ClassCard(1, student, math1SectionA);
-		cc2 = new ClassCard(2, student, math2SectionA);
-
-		//firstTermEnrollmentForm = new EnrollmentForm(1, student);
+		mondayAtTen = new Schedule(DaySlot.MonThu, TimeSlot.TenToElevenThirty);
 	}
 	
 	@Test (expected = ScheduleConflictException.class)
 	public void ScheduleConflict() throws IneligibleStudentException, NoClassCardException, DuplicateClassCardException, ScheduleConflictException, DuplicateSectionException {		
 		firstTermEnrollmentForm = student.startEnrollment();
+		
+		Section math1Section = new Section(1, math1, mondayAtTen, mrNarwhal);
+		Section eng1Section = new Section(2, eng1, mondayAtTen, mrOcelot);
+		
+		ClassCard cc1 = new ClassCard(1, student, math1Section);
+		ClassCard cc2 = new ClassCard(2, student, eng1Section);
+		
 		firstTermEnrollmentForm.addClassCard(cc1);
 		firstTermEnrollmentForm.addClassCard(cc2); //throws ScheduleConflictException
 	}
 	
 	@Test (expected = SectionLimitExceededException.class)
-	public void tooManyStudentsInSection() throws SectionLimitExceededException, DuplicateClassCardException, IneligibleStudentException{
-		fillUpSection();
+	public void tooManyStudentsInSection() throws SectionLimitExceededException, DuplicateClassCardException, IneligibleStudentException, DuplicateSectionException, ScheduleConflictException{
+		Section math1Section = new Section(1, math1, mondayAtTen, mrNarwhal);
+		fillUpSection(math1Section);
 		
 		Student extraStudent = new Student(41, bsMath);
 		extraStudent.startEnrollment();
 		ClassCard extraClassCard = new ClassCard(41);
-		math1SectionA.addClassCard(extraClassCard);
+		math1Section.addClassCard(extraClassCard);
 	}
 	
-	private void fillUpSection() throws SectionLimitExceededException, DuplicateClassCardException, IneligibleStudentException {
+	private void fillUpSection(Section section) throws SectionLimitExceededException, DuplicateClassCardException, IneligibleStudentException {
 		Student anotherStudent;
 		ClassCard anotherClassCard;
 		
@@ -83,12 +76,15 @@ public class EnrollmentTest {
 			anotherStudent = new Student(a, bsMath);
 			anotherStudent.startEnrollment();
 			anotherClassCard = new ClassCard(a);
-			math1SectionA.addClassCard(anotherClassCard);
+			section.addClassCard(anotherClassCard);
 		}
 	}
 	
 	@Test (expected = UnderloadException.class)
-	public void underloadIsNotAllowed() throws IneligibleStudentException, NoClassCardException, UnderloadException, OverloadException, DuplicateClassCardException, ScheduleConflictException{
+	public void underloadIsNotAllowed() throws IneligibleStudentException, NoClassCardException, UnderloadException, OverloadException, DuplicateClassCardException, ScheduleConflictException, DuplicateSectionException{
+		Section math1Section = new Section(1, math1, mondayAtTen, mrNarwhal);
+		ClassCard cc1 = new ClassCard(1, student, math1Section);
+		
 		firstTermEnrollmentForm = student.startEnrollment();
 		firstTermEnrollmentForm.addClassCard(cc1);
 		student.addNewEnrollmentForm(firstTermEnrollmentForm);
@@ -103,7 +99,6 @@ public class EnrollmentTest {
 	
 	private void addTooManyClasscardsToEnrollmentForm() throws IneligibleStudentException, DuplicateClassCardException, ScheduleConflictException, NoClassCardException, UnderloadException, OverloadException, DuplicateSectionException {
 		firstTermEnrollmentForm = student.startEnrollment();
-		firstTermEnrollmentForm.addClassCard(cc1); 
 		
 		Subject psy1 = new Subject("Psychology 1");
 		Subject phlo1 = new Subject("Philosophy 1");
@@ -119,6 +114,7 @@ public class EnrollmentTest {
 		Schedule tuesdayAtTwoThirty = new Schedule(DaySlot.TueFri, TimeSlot.TwoThirtyToFour);
 		Schedule tuesdayAtFour = new Schedule(DaySlot.TueFri, TimeSlot.FourToFiveThirty);
 		
+		Section math1Section = new Section(1, math1, mondayAtTen, mrNarwhal);
 		Section psy1Section = new Section(10, psy1, tuesdayAtEightThirty, mrNarwhal);
 		Section phlo1Section = new Section(11, phlo1, tuesdayAtTen, mrNarwhal);
 		Section eng1Section = new Section(12, eng1, tuesdayAtElevenThirty, mrNarwhal);
@@ -126,6 +122,7 @@ public class EnrollmentTest {
 		Section hist1Section = new Section(14, hist1, tuesdayAtTwoThirty, mrNarwhal);
 		Section frch1Section = new Section(15, frch1, tuesdayAtFour, mrNarwhal);
 		
+		ClassCard cc1 = new ClassCard(1, student, math1Section);
 		ClassCard cc3 = new ClassCard(3, student, psy1Section);
 		ClassCard cc4 = new ClassCard(4, student, phlo1Section);
 		ClassCard cc5 = new ClassCard(5, student, eng1Section);
@@ -133,6 +130,7 @@ public class EnrollmentTest {
 		ClassCard cc7 = new ClassCard(7, student, hist1Section);
 		ClassCard cc8 = new ClassCard(8, student, frch1Section);
 		
+		firstTermEnrollmentForm.addClassCard(cc1);
 		firstTermEnrollmentForm.addClassCard(cc3);
 		firstTermEnrollmentForm.addClassCard(cc4);
 		firstTermEnrollmentForm.addClassCard(cc5);
@@ -140,4 +138,10 @@ public class EnrollmentTest {
 		firstTermEnrollmentForm.addClassCard(cc7);
 		firstTermEnrollmentForm.addClassCard(cc8);
 	}
+	
+	/*
+	@Test (expected = InsufficientPrerequisitesException.class)
+	public void enrollingWithoutPrerequisites() {
+		Subject cs 123 = new 
+	}*/
 }
