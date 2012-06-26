e651490f8df331dc4987fe8f2b3f2e137647d492
Mon <mon.carandang@orangeandbronze.com>
2012-06-26 09:15:55 +0800

    Refactored SectionTests.java. Improved readability.

--- test/com/onb/srs/SectionTests.java
+++ test/com/onb/srs/SectionTests.java
@@ -3,82 +3,57 @@ package com.onb.srs;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.onb.srs.exceptions.DuplicateSectionException;
 import com.onb.srs.exceptions.ScheduleConflictException;
 
 public class SectionTests {
-	Subject cs11;
-	Subject cs21;
-	Subject cs56;
-	Subject cs100;
+	private Subject cs11 = new Subject("CS 11");
+	private Subject cs56 = new Subject("CS 56");
 	
-	Teacher teacher;
+	private Section cs11Section;
+	private Section cs56Section;
 	
-	Section cs11Section;
-	Section cs56Section;
+	private Schedule mondayAtTen = new Schedule(DaySlot.MonThu, TimeSlot.TenToElevenThirty);
 	
-	Schedule mondayAtTen;
-	Schedule mondayAtElevenThirty;
-	Schedule tuesdayAtEightThirty;
-	Schedule tuesdayAtTen;
-	Schedule tuesdayAtOne;
+	private Teacher teacher;
 	
 	@Before
 	public void setUp() {
-		teacher = new Teacher(004316);
-		
-		cs11 = new Subject("CS 11");
-		cs21 = new Subject("CS 21");
-		cs56 = new Subject("CS 56");
-		cs100 = new Subject("CS 100");
-		
-		createFirstSection();
-	}
-	
-	private void createFirstSection() {
-		try {
-			mondayAtTen = new Schedule(DaySlot.MonThu, TimeSlot.TenToElevenThirty);
-			cs11Section = new Section(001, cs11, mondayAtTen, teacher);
-		} catch (ScheduleConflictException e) { }
-		  catch (DuplicateSectionException e) { }
+		teacher = new Teacher(004316); 
 	}
 	
 	@Test
-	public void newSectionHasRequirements() {
+	public void newSectionHasRequirements() throws DuplicateSectionException, ScheduleConflictException {
+		cs11Section = new Section(001, cs11, mondayAtTen, teacher);
 		assertNotNull(cs11Section.getTeacher());
 		assertNotNull(cs11Section.getSubject());
 		assertNotNull(cs11Section.getSchedule());
 	}
 	
 	@Test
-	public void newSectionIsAddedToTeacher() {
+	public void newSectionIsAddedToTeacher() throws DuplicateSectionException, ScheduleConflictException {
+		cs11Section = new Section(001, cs11, mondayAtTen, teacher);
 		assertEquals(1, teacher.getSections().size());
 		assertTrue(teacher.hasSection(cs11Section));
 	}
 	
-	@Test
-	public void creationOfSectionWithConflictIsAborted() {
-		assertNull(cs56Section);
-		createSectionWithConflict();
-		assertNull(cs56Section);
+	@Test (expected = ScheduleConflictException.class)
+	public void creationOfSectionWithConflictIsAborted() throws DuplicateSectionException, ScheduleConflictException {
+		cs11Section = new Section(001, cs11, mondayAtTen, teacher);
+		cs56Section = new Section(002, cs56, mondayAtTen, teacher);
 	}
 	
-	private void createSectionWithConflict(){
+	@Test
+	public void sectionWithScheduleConflictIsNotAddedToTeacher() throws DuplicateSectionException, ScheduleConflictException{
+		cs11Section = new Section(001, cs11, mondayAtTen, teacher);
+		assertEquals(1, teacher.getSections().size());
 		try {
 			cs56Section = new Section(002, cs56, mondayAtTen, teacher);
-		} catch (ScheduleConflictException e) { }
-		catch (DuplicateSectionException e) { }
-	}
-	
-	@Test
-	public void sectionWithScheduleConflictIsNotAddedToTeacher(){
-		assertEquals(1, teacher.getSections().size());
-		createSectionWithConflict();
+		} catch (ScheduleConflictException e) {}
+		catch (DuplicateSectionException e) {}
+		
 		assertEquals(1, teacher.getSections().size());
 	}
-	
-	
 }
